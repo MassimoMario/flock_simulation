073a401f9f8f784becc923feb76ce6fdd69164db
ilmariuolo <massimomario99@gmail.com>
2025-03-01 14:13:04 +0100

    Added two tests
    - test when an invalid type is given for space_length
    
    - test when a negative number is given for space_length

--- test.py
+++ test.py
@@ -4,9 +4,10 @@ import pytest
 import numpy as np
 from flock_class import Flock
 
+random_seed = 1999
 
 def test_invalid_type_N_birds_in_constructor():
-    """Test that the Flock class constructor raises an error when an invalid type as N_birds is provided.
+    """Test that the Flock class constructor raises an error when an invalid type in N_birds argument is provided.
 
     GIVEN: An invalid type for N_birds in the class constructor
 
@@ -18,7 +19,8 @@ def test_invalid_type_N_birds_in_constructor():
     with pytest.raises(TypeError,
                        match = 'Number of birds must be an integer number',
                 ): 
-                    flock = Flock('mille', 100, 1999)
+                    flock = Flock(N_birds = 'mille', space_length = 100, seed = random_seed)
+
 
 
 def test_invalid_value_N_birds_in_constructor():
@@ -34,4 +36,38 @@ def test_invalid_value_N_birds_in_constructor():
     with pytest.raises(ValueError,
                        match = 'Number of birds must be > 0',
                 ): 
-                    flock = Flock(-1, 100, 1999)
+                    flock = Flock(N_birds = -1, space_length = 100, seed = random_seed)
+
+
+
+def test_invalid_type_space_length_in_constructor():
+    """Test that the Flock class constructor raises an error when an invalid type in space_length argument is provided.
+
+    GIVEN: An invalid type for space_length in the class constructor
+
+    WHEN: The constructor is called to create an object Flock
+
+    THEN: A TypeError is raised 
+    """
+
+    with pytest.raises(TypeError,
+                       match = 'Space length must be a floating number',
+                ): 
+                    flock = Flock(N_birds = 200, space_length = 'mille', seed = random_seed)
+
+
+
+def test_invalid_value_space_length_in_constructor():
+    """Test that the Flock class constructor raises an error when a negative number of space length is provided.
+
+    GIVEN: A negative number for space_length in the class constructor
+
+    WHEN: The constructor is called to create an object Flock
+
+    THEN: A ValueError is raised 
+    """
+
+    with pytest.raises(ValueError,
+                       match = 'Space length must be > 0',
+                ): 
+                    flock = Flock(N_birds = 200, space_length = -1, seed = random_seed)
