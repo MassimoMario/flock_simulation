e2e043740f7d498c3f6f89792bb0414dfe449ba8
ilmariuolo <massimomario99@gmail.com>
2025-03-10 10:54:47 +0100

    I forgot plt.show, and I changed the interval parameters of the FuncAnimation

--- animation.py
+++ animation.py
@@ -5,7 +5,7 @@ from IPython.display import display, HTML
 
 
 
-def animate(boid_positions_per_time_step, boid_velocities_per_time_step, space_length, num_time_steps, save = False):
+def animate(birds_positions_per_time_step, birds_velocities_per_time_step, space_length, num_time_steps, save = False):
         """ Animates the simulation creating a video/GIF
 
         Parameters:
@@ -23,7 +23,7 @@ def animate(boid_positions_per_time_step, boid_velocities_per_time_step, space_l
                 Total number of time steps
 
             save : bool, optional
-                Bool variable to save or not the gif produce, default is False
+                Bool variable to save or not the gif produced, default is False
 
         Returns:
         -----------
@@ -32,11 +32,11 @@ def animate(boid_positions_per_time_step, boid_velocities_per_time_step, space_l
 
         fig, ax = plt.subplots(figsize=(7,7))
 
-        velocities_magnitudes = np.linalg.norm(boid_velocities_per_time_step[0], axis=1)
-        velocities_normalized = boid_velocities_per_time_step[0] / np.reshape(velocities_magnitudes, (-1,1))
+        velocities_magnitudes = np.linalg.norm(birds_velocities_per_time_step[0], axis=1)
+        velocities_normalized = birds_velocities_per_time_step[0] / np.reshape(velocities_magnitudes, (-1,1))
         
-        scat = ax.quiver(boid_positions_per_time_step[0][:,0], 
-                        boid_positions_per_time_step[0][:,1],
+        scat = ax.quiver(birds_positions_per_time_step[0][:,0], 
+                        birds_positions_per_time_step[0][:,1],
                         velocities_normalized[:,0],
                         velocities_normalized[:,1], scale=14, scale_units='inches')
         
@@ -44,20 +44,22 @@ def animate(boid_positions_per_time_step, boid_velocities_per_time_step, space_l
         ax.set_ylim([0,space_length])
 
         def update(frame):
-            scat.set_offsets(boid_positions_per_time_step[frame])
+            scat.set_offsets(birds_positions_per_time_step[frame])
 
-            velocities_magnitudes = np.linalg.norm(boid_velocities_per_time_step[frame], axis=1)
-            velocities_normalized = boid_velocities_per_time_step[frame]/ np.reshape(velocities_magnitudes, (-1,1))
+            velocities_magnitudes = np.linalg.norm(birds_velocities_per_time_step[frame], axis=1)
+            velocities_normalized = birds_velocities_per_time_step[frame]/ np.reshape(velocities_magnitudes, (-1,1))
             scat.set_UVC(velocities_normalized[:,0], 
                         velocities_normalized[:,1])
         
 
             return scat,
 
-        ani = FuncAnimation(fig, update, frames=num_time_steps, blit=True)
+        ani = FuncAnimation(fig, update, frames=num_time_steps, blit=True, interval = 25)
         ax.axis('off')
         print("Animation finished. Video processing . . .")
         display(HTML(ani.to_jshtml()))
+
+        plt.show()
 
         if save:
               ani.save('flock_simulation.gif', writer="pillow", fps=60)
