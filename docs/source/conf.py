122f735c4dd08e5e3eea2ee1d8cb9061b366f367
ilmariuolo <massimomario99@gmail.com>
2025-03-19 15:24:18 +0100

    Fixed source folder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../scripts'))
+sys.path.insert(0, os.path.abspath('../../scripts'))
 
 project = 'Flock simulation'
 copyright = '2025, Mario Massimo'
